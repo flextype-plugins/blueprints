878932263d69c6c9351ffe26faaf804ed984db5f
Awilum <sergey.romanenko@flextype.org>
2021-04-29 20:54:14 +0300

    feat(gulp): improve gulp build process
    - show file sizes
    - fix and improve tasks `flatpickr-langs` `trumbowyg-langs`

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ sass.compiler      = require('node-sass');
 /**
  * Task: gulp css
  */
- gulp.task("css", function () {
+gulp.task("css", function () {
     return gulp
         .src([
             // Swal2
@@ -27,7 +27,7 @@ sass.compiler      = require('node-sass');
             'node_modules/trumbowyg/dist/ui/trumbowyg.min.css',
             'node_modules/trumbowyg/dist/plugins/table/ui/trumbowyg.table.css',
 
-            // Flextype UI CSS
+            // Blueprints CSS
             'assets/src/blueprints.scss'
         ])
         .pipe(sass().on('error', sass.logError))
@@ -50,7 +50,7 @@ sass.compiler      = require('node-sass');
 /**
  * Task: gulp js
  */
- gulp.task('js', function () {
+gulp.task('js', function () {
     return gulp
         .src([
             // jQuery
@@ -92,39 +92,42 @@ sass.compiler      = require('node-sass');
         .pipe(rename("blueprints.min.js.gz"))
         .pipe(gulp.dest("assets/dist/js/"))
         .pipe(size({ showFiles: true, gzip: true }));
- });
+});
 
 /**
  * Task: gulp trumbowyg-fonts
  */
- gulp.task('trumbowyg-fonts', function () {
+gulp.task('trumbowyg-fonts', function () {
     return gulp
         .src(['node_modules/trumbowyg/dist/ui/icons.svg'])
+        .pipe(size({ showFiles: true }))
         .pipe(gulp.dest('assets/dist/fonts/trumbowyg'));
- });
+});
 
 /**
  * Task: gulp trumbowyg-langs
  */
- gulp.task('trumbowyg-langs', function () {
+gulp.task('trumbowyg-langs', function () {
     return gulp
-        .src(['node_modules/trumbowyg/dist/*langs/**/*'])
+        .src(['node_modules/trumbowyg/dist/*langs/**/*.min.js'])
+        .pipe(size({ showFiles: true }))
         .pipe(gulp.dest('assets/dist/lang/trumbowyg'));
- });
+});
 
 /**
  * Task: gulp flatpickr-langs
  */
- gulp.task('flatpickr-langs', function () {
+gulp.task('flatpickr-langs', function () {
     return gulp
-        .src(['node_modules/flatpickr/dist/*l10n/**/*'])
+        .src(['node_modules/flatpickr/dist/*l10n/**/*.js'])
+        .pipe(size({ showFiles: true }))
         .pipe(gulp.dest('assets/dist/lang/flatpickr'));
- });
+});
 
 /**
  * Task: gulp default
  */
- gulp.task('default',
+gulp.task('default',
     gulp.series(
         'trumbowyg-fonts',
         'trumbowyg-langs',
@@ -132,14 +135,14 @@ sass.compiler      = require('node-sass');
         'css',
         'js'
     )
- );
+);
 
 /**
  * Task: gulp watch
  */
- gulp.task('watch', function () {
+gulp.task('watch', function () {
     gulp.watch(
         ["blocks/**/*.html", "assets/src/"],
         gulp.series('default')
     );
- });
+});
