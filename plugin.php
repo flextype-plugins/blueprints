4c56e008af1b2efacbe937ec9504187466c001fb
Awilum <sergey.romanenko@flextype.org>
2021-05-05 13:17:41 +0300

    feat(plugin): update assets loading for blueprints

--- plugin.php
+++ plugin.php
@@ -2,12 +2,60 @@
 
 declare(strict_types=1);
 
-namespace Flextype;
+/**
+ * @link https://flextype.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flextype\Plugin\Blueprints;
+
+use Flextype\Plugin\Blueprints\Models\Blueprints;
+use Flextype\Plugin\Twig\Twig\FlextypeTwig;
+use function array_merge;
+use function strtolower;
+use function substr;
+use function is_file;
 
 /**
- * Set base admin route
+ * Ensure vendor libraries exist
  */
-$admin_route = flextype('registry')->get('plugins.admin.settings.route');
+! is_file($blueprints_autoload = __DIR__ . '/vendor/autoload.php') and exit('Please run: <i>composer install</i> form plugin');
 
-include __DIR__ . '/bootstrap.php';
-include __DIR__ . '/routes/web.php';
+/**
+ * Register The Auto Loader
+ *
+ * Composer provides a convenient, automatically generated class loader for
+ * our application. We just need to utilize it! We'll simply require it
+ * into the script here so that we don't have to worry about manual
+ * loading any of our classes later on. It feels nice to relax.
+ * Register The Auto Loader
+ */
+$blueprints_loader = require_once $blueprints_autoload;
+
+/**
+ * Add Blueprints Model to Flextype container
+ */
+flextype()->container()['blueprints'] = fn() => new Blueprints();
+
+/**
+ * Add Blueprints Twig extension
+ */
+FlextypeTwig::macro('blueprints', fn() => flextype('blueprints'));
+
+$blueprintsJS[]  = 'project/plugins/blueprints/assets/dist/js/blueprints.min.js';
+$blueprintsCSS[] = 'project/plugins/blueprints/assets/dist/css/blueprints.min.css';
+
+if (flextype('registry')->get('flextype.settings.locale') == 'en_US') {
+    $locale = 'en';
+} else {
+    $locale = strings(flextype('registry')->get('flextype.settings.locale'))->lower()->substr(0, 2)->toString();
+}
+
+if ($locale !== 'en') {
+    $blueprintsJS[] = 'project/plugins/blueprints/assets/dist/lang/flatpickr/l10n/' . $locale . '.js';          
+} 
+
+flextype('registry')->set('assets.admin.js.blueprints', $blueprintsJS);
+flextype('registry')->set('assets.admin.css.blueprints', $blueprintsCSS);
